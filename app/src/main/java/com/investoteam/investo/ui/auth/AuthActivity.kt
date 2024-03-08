631574570461da931b18781be0ff08761a218f57
hamza <aheita3@gmail.com>
2024-03-08 19:42:32 +0200

    add auth activity

---
+++ app/src/main/java/com/investoteam/investo/ui/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.investoteam.investo.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.investoteam.investo.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
