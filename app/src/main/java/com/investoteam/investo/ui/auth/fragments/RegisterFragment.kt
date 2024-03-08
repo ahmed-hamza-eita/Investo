d10c03bf713877fef54bee2f09547b1558442674
hamza <aheita3@gmail.com>
2024-03-08 20:39:03 +0200

    design register screen

---
+++ app/src/main/java/com/investoteam/investo/ui/auth/fragments/RegisterFragment.kt
@@ -0,0 +1,21 @@
+package com.investoteam.investo.ui.auth.fragments;
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.viewbinding.ViewBinding
+import com.investoteam.investo.databinding.FragmentRegisterBinding
+import com.investoteam.investo.utils.BindingFragment
+
+
+class RegisterFragment : BindingFragment<FragmentRegisterBinding>() {
+    override val bindingInflater: (LayoutInflater) -> ViewBinding
+        get() = FragmentRegisterBinding::inflate
+
+    //private val vm: LoginViewModel by viewModels()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+}
