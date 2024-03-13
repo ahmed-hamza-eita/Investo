a7e42c1a5346754fe16bbf3caa23d405fececd96
hamza <aheita3@gmail.com>
2024-03-13 16:35:11 +0200

    helper class to manage api requests

---
+++ app/src/main/java/com/investoteam/investo/utils/NetworkState.kt
@@ -0,0 +1,47 @@
+package com.investoteam.investo.utils;
+
+import retrofit2.HttpException
+import java.io.IOException
+import java.net.SocketTimeoutException
+
+class NetworkState constructor(val status: Status, val msg: Any? = null, val data: Any? = null) {
+
+    enum class Status {
+        RUNNING, FAILED, SUCCESS
+    }
+
+    companion object {
+        fun getLoaded(dataSuccess: Any?): NetworkState {
+            return NetworkState(Status.SUCCESS, data = dataSuccess)
+        }
+
+        var LOADING: NetworkState = NetworkState(Status.RUNNING)
+
+        fun getErrorMessage(throwable: Throwable): NetworkState {
+            return when (throwable) {
+                is IOException -> {
+                    NetworkState(Status.FAILED, "No Connection ")
+                }
+
+                is SocketTimeoutException -> {
+                    NetworkState(Status.FAILED, "Bad Connection")
+                }
+
+                is HttpException -> {
+
+                    NetworkState(Status.FAILED, "HttpException  ")
+                }
+
+                else -> {
+                    NetworkState(Status.FAILED, "Error")
+                }
+            }
+        }
+
+        fun getErrorMessage(massage: String): NetworkState {
+            return NetworkState(Status.FAILED, massage)
+
+        }
+
+    }
+} // class of NetworkState
